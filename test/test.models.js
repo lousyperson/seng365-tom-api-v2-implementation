1b5373f196af8473dfaa751f3ec5ae70cd2c212e
Thomas Young <thomas.young@canterbury.ac.nz>
2017-09-21 22:56:08 +1200

    Fixed model tests for api v2.1 /project query parameters
    Tests will fail against implementation as it currently lacks these parameters

--- test/test.models.js
+++ test/test.models.js
@@ -22,32 +22,20 @@ const
     pledges = require('../app/models/pledges.model'),
     db = require('../app/lib/db'),
     initDb = require('../app/lib/db.init'),
-    validator = require('../app/lib/validator'),
-    apiSchema = require('../config/api.schema');
+    validator = require('../app/lib/validator');
 
-const userTemplate = (username, email, password) => {
-    return {
-        user: {
-            id: 0,
-            username: username,
-            location: "Valhalla",
-            email: email
-        },
-        password: password
-    };
-};
 
-const projectTemplate = (creator, id) => {
+const projectTemplate = (username, creatorId) => {
     return {
         title: "My awesome project",
         subtitle: "More awesomeness",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor inccreatorIdcreatorIdunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupcreatorIdatat non procreatorIdent, sunt in culpa qui officia deserunt mollit anim creatorId est laborum.",
         imageUri: "/projects/0/image",
         target: 123400,
         creators: [
             {
-                id: id,
-                name: creator
+                id: creatorId,
+                name: username
             }
         ],
         rewards: rewardsTemplate()
@@ -85,23 +73,32 @@ const createUser = user => {
     return new Promise((resolve, reject) => {
         users.insert(user, (err, userId) => {
             if (err) return reject(err);
-            return resolve(err, userId)
+            return resolve(userId);
         })
     })
 };
 
 const createProject = project => {
-}
-}
+    return new Promise((resolve, reject) => {
+        projects.insert(project, (err, projectId) => {
+            if (err) return reject(err);
+            return resolve(projectId);
+        })
+    })
+};
 
-
+const pledgeToProject = (projectId, userId) => {
+    return new Promise((resolve, reject) => {
+        pledges.insert(projectId, pledgeTemplate(userId, false, 250), (err, pledgeId) => {
+            if (err) return reject(err);
+            return resolve(pledgeId);
+        })
+    })
+};
 
 describe('given a clean db', function() {
-
-    let userId;
-
+    
     beforeEach(`clean db`, function() {
-        this.timeout(5000);
         return initDb(config.get('db'))
             .catch(err => {
                 console.log(err);
@@ -116,33 +113,24 @@ describe('given a clean db', function() {
         })
     });
 
-    beforeEach(`create user to prevent foreign key issues`, function(done) {
-        users.insert({username: 'loki', email:'loki@valhalla.biz', password:'toki'}, (err, result) => {
-            if (err) return done(err);
-            userId = result;
-            return done(err, result)
-        })
-    });
-
-    afterEach('close connection', function(done) {
-        db.end(done);
-    });
-
     describe('With a single user', function(done) {
 
-        it('insert project', function (done) {
-            let project = projectTemplate('loki', userId);
-            projects.insert(project, (err, results) => {
-                should.equal(err, null);
-                results.should.equal(1);
-                return done()
-            })
+        let user1Id;
+        
+        beforeEach(`create user`, function() {
+            return createUser({username: 'loki', email:'loki@valhalla.biz', password:'toki'})
+                .then(_id => user1Id = _id)
+        });
+        
+        it('insert project', function () {
+            return createProject(projectTemplate('loki', user1Id))
+                .then(id => id.should.equal(1))
         });
 
         it('delete user and check auth', function (done) {
             users.authenticate('loki', 'toki', result => {
                 result.should.be.true;
-                users.remove(userId, (err, results) => {
+                users.remove(user1Id, (err, results) => {
                     users.authenticate('loki', 'toki', result => {
                         result.should.be.false;
                         return done();
@@ -154,7 +142,7 @@ describe('given a clean db', function() {
         it('delete user and check idFromToken', function (done) {
             users.authenticate('loki', 'toki', result => {
                 result.should.be.true;
-                users.remove(userId, (err, results) => {
+                users.remove(user1Id, (err, results) => {
                     users.authenticate('loki', 'toki', result => {
                         result.should.be.false;
                         return done();
@@ -164,11 +152,11 @@ describe('given a clean db', function() {
         });
 
         it('delete user and check not shown', function (done) {
-            users.remove(userId, () => {
-                users.getOne(userId, true, (err, user) => {  // only active users
+            users.remove(user1Id, () => {
+                users.getOne(user1Id, true, (err, user) => {  // only active users
                     should.equal(user, null);
-                    users.getOne(userId, false, (err, user) => {  // include deleted users
-                        user.id.should.equal(userId);
+                    users.getOne(user1Id, false, (err, user) => {  // include deleted users
+                        user.id.should.equal(user1Id);
                         return done();
                     })
                 })
@@ -179,18 +167,17 @@ describe('given a clean db', function() {
 
     describe('With a user and a project', function(done) {
 
-        let projectId;
+        let user1Id, project1Id;
 
-        beforeEach('Create project', function(done) {
-            let project = projectTemplate('loki', userId);
-            projects.insert(project, (err, _projectId) => {
-                projectId = _projectId;
-                return done()
-            })
+        beforeEach('Create project', function() {
+            return createUser({username: 'loki', email:'loki@valhalla.biz', password:'toki'})
+                .then(_id => user1Id = _id)
+                .then(() => createProject(projectTemplate('loki', user1Id)))
+                .then(_id => project1Id = _id);
         });
 
         it('get project', function (done) {
-            projects.getOne(projectId, (err, project) => {
+            projects.getOne(project1Id, (err, project) => {
                 should.equal(err, null);
                 validator.isValidSchema(project, 'definitions.ProjectDetails');
                 return done();
@@ -206,14 +193,14 @@ describe('given a clean db', function() {
         });
 
         it('update rewards', function(done) {
-            rewards.update(projectId, rewardsTemplate(), err => {
+            rewards.update(project1Id, rewardsTemplate(), err => {
                 should.equal(err, null);
                 return done();
             })
         });
 
         it('make pledge', function(done) {
-            pledges.insert(projectId, pledgeTemplate(), (err, id) => {
+            pledges.insert(project1Id, pledgeTemplate(), (err, id) => {
                 should.equal(err, null);
                 Number.isInteger(id).should.be.true;
                 return done();
@@ -221,8 +208,8 @@ describe('given a clean db', function() {
         });
 
         it('get anonymous pledges', function(done) {
-            pledges.insert(projectId, pledgeTemplate(1, true), (err, id) => {
-                projects.getOne(projectId, (err, project) => {
+            pledges.insert(project1Id, pledgeTemplate(1, true), (err, id) => {
+                projects.getOne(project1Id, (err, project) => {
                     should.equal(err, null);
                     validator.isValidSchema(project, 'definitions.ProjectDetails').should.be.true;
                     project.backers[0].username.should.equal("anonymous");
@@ -239,7 +226,7 @@ describe('given a clean db', function() {
         });
 
         it('put status change', function (done) {
-            projects.update(projectId, false, err => {
+            projects.update(project1Id, false, err => {
                 should.equal(err, null);
                 // TODO: check that status has changed to closed
                 return done();
@@ -250,26 +237,17 @@ describe('given a clean db', function() {
 
     describe('With a user and two projects', function(done) {
 
-        let project1Id, project2Id;
+        let user1Id, project1Id, project2Id;
 
-        beforeEach('Create project', function(done) {
-            let project = projectTemplate('loki', userId);
-            project = Object.assign(project, {title: "Project1"});
-            projects.insert(project, (err, _projectId) => {
-                setTimeout(() => {
-                    project1Id = _projectId;
-                    project = Object.assign(project, {title: "Project2"});
-                    projects.insert(project, (err, _projectId) => {
-                        project2Id = _projectId;
-                        users.insert({username: 'loki', email:'loki@valhalla.biz', password:'toki'}, (err, result) => {
-                            if (err) return done(err);
-                            userId = result;
-                            return done(err, result)
-                        })
-                    })
-                }, 1000)
-
-            })
+        beforeEach('Create project', function() {
+            let project;
+            return createUser({username: 'loki', email:'loki@valhalla.biz', password:'toki'})
+                .then(_id => user1Id = _id)
+                .then(() => project = projectTemplate('loki', user1Id))
+                .then(() => createProject(Object.assign(project, {title: "Project1"})))
+                .then(_id => project1Id = _id)
+                .then(() => createProject(Object.assign(project, {title: "Project2"})))
+                .then(_id => project2Id = _id)
         });
 
         it('get projects', function (done) {
@@ -281,22 +259,22 @@ describe('given a clean db', function() {
             })
         });
 
-        it('get projects with offset 1', function (done) {
+        it('get projects with offset 1 (last project)', function (done) {
             projects.getAll({limit:10, offset:1}, (err, results) => {
                 should.equal(err, null);
                 results.should.have.lengthOf(1);
                 validator.isValidSchema(results, 'definitions.ProjectsOverview').should.be.true;
-                results[0].title.should.equal('Project1'); // ordered from most recent to least recent
+                results[0].title.should.equal('Project2'); // ordered from least recent to most recent
                 return done();
             })
         });
 
-        it('get projects with limit 1', function (done) {
+        it('get projects with limit 1 (first project)', function (done) {
             projects.getAll({limit:1}, (err, results) => {
                 should.equal(err, null);
                 results.should.have.lengthOf(1);
                 validator.isValidSchema(results, 'definitions.ProjectsOverview').should.be.true;
-                results[0].title.should.equal('Project2'); // ordered from most recent to least recent
+                results[0].title.should.equal('Project1'); // ordered from least recent to most recent
                 return done();
             })
         });
@@ -311,7 +289,7 @@ describe('given a clean db', function() {
         });
 
         it('check totals', function (done) {
-            pledges.insert(project1Id, pledgeTemplate(userId, false, 250), (err, id) => {
+            pledges.insert(project1Id, pledgeTemplate(user1Id, false, 250), (err, id) => {
                 pledges.getTotals(project1Id, (err, totals) => {
                     should.not.equal(totals, null);
                     totals.total.should.equal(250);
@@ -325,40 +303,44 @@ describe('given a clean db', function() {
 
     describe('With a two users each with one project, each pledging to the other project', function(done) {
 
-        let project1Id, project2Id;
+        let user1Id, user2Id, project1Id, project2Id;
 
-        beforeEach('Create project', function(done) {
-            let project = projectTemplate('loki', userId);
-            project = Object.assign(project, {title: "Project1"});
-            projects.insert(project, (err, _projectId) => {
-                setTimeout(() => {
-                    project1Id = _projectId;
-                    project = Object.assign(project, {title: "Project2"});
-                    projects.insert(project, (err, _projectId) => {
-                        project2Id = _projectId;
-                        return done()
-                    })
-                }, 1000)
+        beforeEach('Create project', function() {
+            return createUser({username: 'loki', email:'loki@valhalla.biz', password:'toki'})
+                .then(_id => user1Id = _id)
+                .then(() => createUser({username: 'toki', email:'toki@valhalla.biz', password:'loki'}))
+                .then(_id => user1Id = _id)
+                .then(() => createProject(projectTemplate('loki', user1Id))) // user1 creates project1
+                .then(_id => project1Id = _id)
+                .then(() => createProject(projectTemplate('toki', user2Id))) // user2 creates project2
+                .then(_id => project2Id = _id)
+                .then(() => pledgeToProject(project1Id, user2Id))  // user2 pledges to project1
+                .then(() => pledgeToProject(project2Id, user1Id))  // user1 pledges to project2
+        });
 
+        it('get projects with creator=1 (loki) should return just project1', function (done) {
+            projects.getAll({creator: user1Id}, (err, results) => {
+                should.equal(err, null);
+                results.should.have.lengthOf(1);
+                validator.isValidSchema(results, 'definitions.ProjectsOverview').should.be.true;
+                results[0].creators[0].username.should.equal('loki');
+                results[0].backers[0].id.should.equal(user1Id);
+                results[0].id.should.equal(project1Id);
+                return done();
             })
         });
 
-    it('get projects with creator=loki', function (done) {
-        projects.getAll({creator:userId}, (err, results) => {
-            should.equal(err, null);
-            results.should.have.lengthOf(2);
-            validator.isValidSchema(results, 'definitions.ProjectsOverview').should.be.true;
-            return done();
-        })
-    });
+        it('get projects with backer=1 (loki) should return just project2', function (done) {
+            projects.getAll({backer: user1Id}, (err, results) => {
+                should.equal(err, null);
+                results.should.have.lengthOf(1);
+                validator.isValidSchema(results, 'definitions.ProjectsOverview').should.be.true;
+                results[0].backers[0].username.should.equal('loki');
+                results[0].backers[0].id.should.equal(user1Id);
+                results[0].id.should.equal(project2Id);
+                return done();
+            })
+        });
 
-    it('get projects with creator=0', function (done) {
-        projects.getAll({creator:true}, (err, results) => {
-            should.equal(err, null);
-            results.should.have.lengthOf(2);
-            validator.isValidSchema(results, 'definitions.ProjectsOverview').should.be.true;
-            return done();
-        })
     });
-
 });
